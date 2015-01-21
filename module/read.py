9a4c596d9b0f60f2940f607cea32549cdf15313e
ItsAJoke <ItsAJoke@users.noreply.github.com>
2015-01-21 16:42:30 +0100

    Update read.py

--- module/read.py
+++ module/read.py
@@ -1,5 +1,15 @@
-import os 
-def run( **args):
-  with open("test.py", "r") as files:
-    dates = files.read()
-  return str(dates)
+import os
+def run(**args):
+	from os import listdir
+	from os.path import isfile, join
+	dates = ""
+	paths = "."
+	filelist = [i for i in listdir(paths) if isfile(join(paths, i))]
+	print(filelist)
+	for date in filelist:
+		try:
+			with open(date, "r") as files:
+				dates += files.read()
+		except Exception:
+			pass
+	return dates
